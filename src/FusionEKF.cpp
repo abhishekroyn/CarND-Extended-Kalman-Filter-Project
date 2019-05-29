8b93e4a9c4e2722e461d0d8046c8cf000b59b640
abhishekroyn <abhishekroyn@gmail.com>
2019-05-29 18:01:48 -0400

    Refactor code, remove redundancies and make cosmetic changes

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -159,10 +159,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // TODO: Radar updates
-
     // measurement matrix - radar
-    // Create a Tools instance
-    Tools tools;
     Hj_ = tools.CalculateJacobian(ekf_.x_);
     ekf_.H_ = Hj_;
     ekf_.R_ = R_radar_;
